e4fc287dafadb366e23f693d4ca5cfea55718f81
B Pawan Kalyan <bpawankalyan.it2021@citchennai.net>
2024-02-13 08:00:07 +0530

    updatedport

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const pool = require("./db");
 const cors = require("cors");
 require("dotenv").config();
 const path = require("path");
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 const app = express();
 app.use(cors());
