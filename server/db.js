eb9724adcde7a39b227ab90d3d6f66fb3d51a7ac
B Pawan Kalyan <115493304+Pawankalyan2023@users.noreply.github.com>
2024-02-27 12:00:15 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,23 +2,23 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 
-// const pool = new Pool({
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     database: process.env.DB_DATABASE,
-// });
-
-
 const pool = new Pool({
-  connectionLimit: process.env.AWS_CONNECTIONLIMIT,
-  user: process.env.AWS_USER,
-  host: process.env.AWS_HOST,
-  database: process.env.AWS_DATABASE,
-  password: process.env.AWS_PASSWORD,
-  port: process.env.AWS_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
 });
+
+
+// const pool = new Pool({
+//   connectionLimit: process.env.AWS_CONNECTIONLIMIT,
+//   user: process.env.AWS_USER,
+//   host: process.env.AWS_HOST,
+//   database: process.env.AWS_DATABASE,
+//   password: process.env.AWS_PASSWORD,
+//   port: process.env.AWS_PORT,
+// });
 
 pool.connect((err, client, release) => {
   if (err) {
