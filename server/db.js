f659acb1da1a05920ea5a61bc9c4ac9ce31521a0
B Pawan Kalyan <115493304+Pawankalyan2023@users.noreply.github.com>
2024-03-18 13:20:05 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
+    user: postgres,
+    password: DbXUerGAspGNKkRvHzKYsRjIPPlZzbRp,
+    host: viaduct.proxy.rlwy.net,
+    port: 29286,
+    database: railway,
 });
 
 
